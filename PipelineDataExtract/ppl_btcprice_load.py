438160eba9b864a2565c0e70e8f1ac6b4e9ed8d1
Ismael.oltra <Ismael.oltra@arinco.com.au>
2023-03-22 15:10:06 +1100

    fixed script

--- PipelineDataExtract/ppl_btcprice_load.py
+++ PipelineDataExtract/ppl_btcprice_load.py
@@ -4,21 +4,22 @@ import mysql.connector
 import json
 
 
-
-
-# Load the credentials file
-with open(os.environ['CREDENTIALS_FILE_PATH']) as f:
-    credentials = json.load(f)
+# Open the file for reading
+with open('../credentials.json', 'r') as f:
+    # Read the contents of the file
+    contents = json.load(f)
 
 # Get the username and password from the environment variables
-username = os.environ['USERNAME']
-password = os.environ['PASSWORD']
+username = contents['DB_USER']
+password = contents['DB_PASSWORD']
+dbname   = contents['DB_NAME']
+
 
 mydb = mysql.connector.connect(
   host="localhost",
   user=username,
   password=password,
-  database="btc_db"
+  database=dbname
 )
 
 response = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
